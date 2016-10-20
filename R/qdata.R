86c7609a1baa979cd77627e4a64c629b088baa70
Veronica Giro <veronica.giro@quantide.com>
2016-10-20 14:19:59 +0200

    modify structure of qdata

---
+++ R/qdata.R
@@ -0,0 +1,25 @@
+#' Quantide Datasets Collection
+#' 
+#' @docType package
+#' 
+#' @name qdata
+#' 
+#' @description 
+#' This package contains several datasets used during \href{http://www.quantide.com}{Quantide} courses.
+#'
+#'
+#' @author
+#' Enrico Tonini \email{enrico.tonini@@quantide.com} \cr
+#' Nicola Sturaro \email{nicola.sturaro@@quantide.com} \cr
+#' Enrico Pegoraro \email{enrico.pegoraro@@quantide.com} \cr
+#' Veronica Giro \email{veronica.giro@@quantide.com} 
+#'
+#' Maintainer: Nicola Sturaro \email{nicola.sturaro@@quantide.com}
+#' 
+#'   
+#' @examples
+#' data(bands)
+#' data(bank)
+#' data(italia)
+#' data(wwiileaders)
+NULL
